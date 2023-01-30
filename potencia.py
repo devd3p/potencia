8525a4e4f7dffc280146547c1bbd629d5fe806f7
Vinicius de Souza <110186039+devd3p@users.noreply.github.com>
2023-01-30 16:41:33 -0300

    Add files via upload

---
+++ potencia.py
@@ -0,0 +1,5 @@
+def potencia(valor,pot):
+    '''Potenciação'''
+    valor = int(input('Digite um valor: '))
+    pot = int(input('Quer elevar este número a qual potência? '))
+    return (valor **pot)
